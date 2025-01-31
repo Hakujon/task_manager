234e90ae30421533b98eafddea1efb66abb8140b
Hakujona <stlesha1992@gmail.com>
2025-01-31 23:11:03 +0300

    add new commands to main and fixed modules

--- main.py
+++ main.py
@@ -17,6 +17,9 @@ class TaskManagerShell(cmd.Cmd):
     def do_load(self, args=None):
         Storage().load_tasks_from_file()
 
+    def do_save(self, args=None):
+        Storage().save_tasks_to_file()
+
     def do_add(self, args):
         description = args.replace('"', '')
         print(description)
@@ -27,7 +30,18 @@ class TaskManagerShell(cmd.Cmd):
         return True
 
     def do_show(self, args):
-        print(Storage.__dir__)
+        all_tasks: dict = Storage().show_all_task()
+        for task in all_tasks.values():
+            print(task)
+
+    def do_update(self, args):
+        args = args.split()
+        id = int(args[0])
+        description = args[1].replace('"', '')
+        print(args)
+        print(id)
+        print(description)
+        Storage().update_task_by_id(id, new_descr=description)
 
 
 if __name__ == "__main__":
--- modules/storage.py
+++ modules/storage.py
@@ -5,7 +5,7 @@ from .task_model import Task
 
 class DatafileStorage:
     __file_path: str = 'tasks.json'
-    __tasks: dict = {}
+    __tasks: dict[int:Task] = {}
 
     def add_task(self, description: str):
         if description:
@@ -22,9 +22,14 @@ class DatafileStorage:
     def load_tasks_from_file(cls):
         try:
             with open(cls.__file_path, 'r') as dump_file:
-                tasks = json.load(dump_file)
-                for key, value in tasks.items():
-                    cls.__tasks[int(key)] = Task.transform_from_dict(value)
+                tasks: dict = json.load(dump_file)
+                if tasks:
+                    for key, value in tasks.items():
+                        cls.__tasks[int(key)] = Task.transform_from_dict(value)
+                    for _ in range(int(sorted(tasks.keys())[-1])):
+                        Task.make_next_id()
+                else:
+                    raise ModuleNotFoundError('you have no dumps')
         except FileNotFoundError('you have no dumps'):
             pass
 
@@ -35,6 +40,10 @@ class DatafileStorage:
             for key, value in cls.__tasks.items()}
         with open(cls.__file_path, 'w+') as file:
             json.dump(dict_of_tasks, file)
+
+    def update_task_by_id(self, id: int, new_descr: str):
+        task: Task = self.get_task_by_id(id)
+        task.update_task(description=new_descr)
 
     def close(self):
         self.reload()
